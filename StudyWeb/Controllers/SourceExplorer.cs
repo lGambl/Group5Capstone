430c0b9ead4458968f1d749210da7d9b04105b80
lGambl <110194340+lGambl@users.noreply.github.com>
2024-01-24 13:17:15 -0500

    Add source creation and video viewing

--- StudyWeb/Controllers/SourceExplorer.cs
+++ StudyWeb/Controllers/SourceExplorer.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using StudyWeb.Data;
 using StudyWeb.Models;
@@ -40,21 +41,187 @@ namespace StudyWeb.Controllers
         [Route("View/{id?}")]
         public async Task<IActionResult> View(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            
+            var source = RetreiveSource(id);
 
-            var source = await _context.Source.FirstOrDefaultAsync(m => m.Id == id);
             if (source == null)
             {
                 return NotFound();
             }
-            
 
             return View(source);
             
         }
+
+        [Authorize]
+        [Route("Source/{id?}")]
+        public Source? GetSource(int? id)
+        {
+
+            var source = RetreiveSource(id);
+            if (source == null)
+            {
+                return null;
+            }
+
+            return source;
+            
+        }
+
+        private Source? RetreiveSource(int? id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+
+            var source = _context.Source.FirstOrDefault(m => m.Id == id);
+
+            if (source == null)
+            {
+                return null;
+            }
+            
+
+            return source;
+        }
+
+        [Authorize]
+        [Route("Create")]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> Create([Bind("Title")] string title, [Bind("Link")] string link, IFormFile pdfUpload, IFormFile videoUpload, [Bind("Type")] SourceTypes type)
+        {
+            var owner = User.Claims.FirstOrDefault().Value;
+            
+
+            if (owner == null || title == null || type == null )
+            {
+                return BadRequest();
+            }
+
+            switch (type)
+            {
+                case SourceTypes.Pdf:
+                    if (pdfUpload == null)
+                    {
+                        return BadRequest();
+                    }
+
+                    if (!await saveFile(pdfUpload, title, owner, type))
+                    {
+                        return BadRequest();
+                    }
+                    break;
+                case SourceTypes.Video:
+                    if (videoUpload == null)
+                    {
+                        return BadRequest();
+                    }
+
+                    if (!await saveFile(videoUpload, title, owner, type))
+                    {
+                        return BadRequest();
+                    }
+                    break;
+                case SourceTypes.PdfLink:
+                    await this.AddLink(title, owner, link, type);
+                    break;
+                case SourceTypes.VideoLink:
+                    if (link == null)
+                    {
+                        return BadRequest();
+                    }
+
+                    if (!await this.AddLink(title, owner, link, type))
+                    {
+                        return BadRequest();
+                    }
+                    break; 
+                default:
+                    return BadRequest();
+                    
+            }
+
+            if (Request.Headers["Accept"] == "application/json")
+            {
+                return Ok(new { success = true, message = "Source created successfully." });
+            }
+
+            return RedirectToAction("Index","SourceExplorer");
+
+        }
+
+        private async Task<bool> saveFile(IFormFile file,string title, string owner, SourceTypes type)
+        {
+            
+
+            var uploadsFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+
+            // Ensure the upload folder exists
+            if (!Directory.Exists(uploadsFolderPath))
+            {
+                Directory.CreateDirectory(uploadsFolderPath);
+            }
+
+            string uniqueFileName = Guid.NewGuid() + "_" + file.FileName;
+            var filePath = Path.Combine(uploadsFolderPath, uniqueFileName);
+
+            try
+            {
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            string relativePath = "uploads";
+
+            var relativeFilePath = Path.Combine(relativePath, uniqueFileName);
+
+            await this.AddLink(title, owner, relativeFilePath, type);
+
+            return true;
+        }
+
+
+        private async Task<bool> AddLink(string title, string owner, string link, SourceTypes type)
+        {
+            try
+            {
+                var insertSourceQuery = "INSERT INTO Source (title, owner, link, type) VALUES (@title, @owner, @link, @type);";
+                SqlParameter[] parameters =
+                {
+                    new SqlParameter("@title", title),
+                    new SqlParameter("@owner", owner),
+                    new SqlParameter("@link", link),
+                    new SqlParameter("@type", type)
+                };
+
+                await _context.Database.ExecuteSqlRawAsync(insertSourceQuery, parameters);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it as per your requirement
+                // For debugging: throw; or return false;
+                return false;
+            }
+
+            return false;
+        }
+
+
 
         /*/// <summary>
         ///   Shows the search results.
