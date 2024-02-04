01c7da7e93489b1c55e4e921e7be7c76fa25d9f7
kmitche9 <74280045+kmitche9@users.noreply.github.com>
2024-02-04 17:18:56 -0500

    Added the Note & Source model classes.
    I added the Note & Source model classes in the desktop application. I also added documentation to the SourceType model class.

--- StudyDesk/Model/SourceType.cs
+++ StudyDesk/Model/SourceType.cs
@@ -1,17 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StudyDesk.Model
+﻿namespace StudyDesk.Model
 {
+    /// <summary>
+    ///   The Source Type Enum
+    /// </summary>
     public enum SourceType
     {
+        /// <summary>
+        ///   The video
+        /// </summary>
         Video = 1,
+
+        /// <summary>
+        ///   The video link
+        /// </summary>
         VideoLink = 2,
+
+        /// <summary>
+        ///   The PDF
+        /// </summary>
         Pdf = 3,
+
+        /// <summary>
+        ///   The PDF link
+        /// </summary>
         PdfLink = 4,
+
+        /// <summary>
+        ///   The image
+        /// </summary>
         Image = 5
     }
 }
