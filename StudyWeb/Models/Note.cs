2dc3962f9335c30968d37f4008728ece92d65f57
lGambl <110194340+lGambl@users.noreply.github.com>
2024-02-04 04:12:17 -0500

    Update Note model

--- StudyWeb/Models/Note.cs
+++ StudyWeb/Models/Note.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string Text { get; set; } = "";
 
-        public int DocumentId { get; set; }
+        public int SourceId { get; set; }
+
+        public string Owner { get; set; } = "";
     }
 }
