56906382dbd9951bb67459ceefa966e361b360c9
Bryson Frederick <93341535+bfreder1@users.noreply.github.com>
2024-02-07 11:59:00 -0500

    Tested Source Model
    The web model for sources now has 100% test coverage.

---
+++ StudyHallTests/StudyWebTests/model/SourceTest.cs
@@ -0,0 +1,65 @@
+﻿using StudyWeb.Models;
+
+namespace StudyHallTests.StudyWebTests.model;
+
+[TestFixture]
+public class SourceTest
+{
+
+    [Test]
+    public void TestDefaultValues()
+    {
+        var source = new Source();
+        Assert.Multiple(() =>
+        {
+            Assert.That(source.Id, Is.EqualTo(0));
+            Assert.That(source.Link, Is.EqualTo(""));
+            Assert.That(source.Title, Is.EqualTo(""));
+            Assert.That(source.Notes, Is.Not.Null);
+            Assert.That(source.Notes, Is.Empty);
+            Assert.That(source.Owner, Is.EqualTo(""));
+
+            Assert.Throws<ArgumentException>(() => source.SourceTypeToString());
+            
+        });
+    }
+
+    [Test]
+    public void TestSourceTypeToString()
+    {
+        var source = new Source { Type = SourceTypes.ImageLink };
+        Assert.That(source.SourceTypeToString(), Is.EqualTo("imageLink"));
+        source = new Source { Type = SourceTypes.Pdf };
+        Assert.That(source.SourceTypeToString(), Is.EqualTo("pdf"));
+        source = new Source { Type = SourceTypes.PdfLink };
+        Assert.That(source.SourceTypeToString(), Is.EqualTo("pdfLink"));
+        source = new Source { Type = SourceTypes.Image };
+        Assert.That(source.SourceTypeToString(), Is.EqualTo("image"));
+        source = new Source { Type = SourceTypes.Video };
+        Assert.That(source.TypeString, Is.EqualTo("video"));
+        source = new Source { Type = SourceTypes.VideoLink };
+        Assert.That(source.TypeString, Is.EqualTo("videoLink"));
+    }
+
+    [Test]
+    public void TestSetters()
+    {
+        var source = new Source
+        {
+            Id = 1,
+            Link = "http://www.google.com",
+            Title = "Google",
+            Type = SourceTypes.Image,
+            Owner = "Me",
+            Notes = new List<Note>()
+        };
+        Assert.Multiple(() =>
+        {
+            Assert.That(source.Id, Is.EqualTo(1));
+            Assert.That(source.Link, Is.EqualTo("http://www.google.com"));
+            Assert.That(source.Title, Is.EqualTo("Google"));
+            Assert.That(source.Type, Is.EqualTo(SourceTypes.Image));
+            Assert.That(source.Owner, Is.EqualTo("Me"));
+        });
+    }
+}
--- StudyWeb/Models/Source.cs
+++ StudyWeb/Models/Source.cs
@@ -1,108 +1,107 @@
-﻿using System.Collections;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 
-namespace StudyWeb.Models
+namespace StudyWeb.Models;
+
+/// <summary>
+///     Source types of sources
+/// </summary>
+public enum SourceTypes
 {
     /// <summary>
-    /// Source types of sources
+    ///     A video
     /// </summary>
-    public enum SourceTypes{
-
-        /// <summary>
-        /// A video
-        /// </summary>
-        Video = 1,
-
-        /// <summary>
-        /// A link to a video
-        /// </summary>
-        VideoLink = 2,
-
-        /// <summary>
-        /// A pdf
-        /// </summary>
-        Pdf = 3,
-
-        /// <summary>
-        /// A link to a pdf
-        /// </summary>
-        PdfLink = 4,
-
-        /// <summary>
-        /// An image
-        /// </summary>
-        Image = 5,
-
-        /// <summary>
-        /// A link to an image
-        /// </summary>
-        ImageLink = 6
-    }
+    Video = 1,
 
     /// <summary>
-    /// A source of information
+    ///     A link to a video
     /// </summary>
-    public class Source
+    VideoLink = 2,
+
+    /// <summary>
+    ///     A pdf
+    /// </summary>
+    Pdf = 3,
+
+    /// <summary>
+    ///     A link to a pdf
+    /// </summary>
+    PdfLink = 4,
+
+    /// <summary>
+    ///     An image
+    /// </summary>
+    Image = 5,
+
+    /// <summary>
+    ///     A link to an image
+    /// </summary>
+    ImageLink = 6
+}
+
+/// <summary>
+///     A source of information
+/// </summary>
+public class Source
+{
+    #region Properties
+
+    /// <summary>
+    ///     Id of Source
+    /// </summary>
+    public int Id { get; init; }
+
+    /// <summary>
+    ///     Link to the source
+    /// </summary>
+    public string Link { get; set; } = "";
+
+    /// <summary>
+    ///     Title of the source
+    /// </summary>
+    public string Title { get; init; } = "";
+
+    /// <summary>
+    ///     Type of source
+    /// </summary>
+    public SourceTypes Type { get; init; }
+
+    /// <summary>
+    ///     Type of source as a string
+    /// </summary>
+    public string TypeString => this.SourceTypeToString();
+
+    /// <summary>
+    ///     Owner of the source
+    /// </summary>
+    public string Owner { get; init; } = "";
+
+    /// <summary>
+    ///     Notes on the source
+    /// </summary>
+    public ICollection<Note> Notes { get; set; } = new Collection<Note>();
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    ///     Convert source type to string
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public string SourceTypeToString()
     {
-        /// <summary>
-        /// Id of Source
-        /// </summary>
-        public int Id { get; set; }
-
-        /// <summary>
-        /// Link to the source
-        /// </summary>
-        public string Link { get; set; } = "";
-
-        /// <summary>
-        /// Title of the source
-        /// </summary>
-        public string Title { get; set; } = "";
-
-        /// <summary>
-        /// Type of source
-        /// </summary>
-        public SourceTypes Type { get; set; }
-
-        /// <summary>
-        /// Type of source as a string
-        /// </summary>
-        public string TypeString => SourceTypeToString();
-
-        /// <summary>
-        /// Owner of the source
-        /// </summary>
-        public string Owner { get; set; } = "";
-
-        /// <summary>
-        /// Notes on the source
-        /// </summary>
-        public ICollection<Note> Notes { get; set; } = new Collection<Note>();
-
-        /// <summary>
-        /// Convert source type to string
-        /// </summary>
-        /// <returns></returns>
-        /// <exception cref="ArgumentException"></exception>
-        public string SourceTypeToString()
+        return this.Type switch
         {
-            switch (this.Type)
-            {
-                case SourceTypes.Video:
-                    return "video";
-                case SourceTypes.VideoLink:
-                    return "videoLink";
-                case SourceTypes.Pdf:
-                    return "pdf";
-                case SourceTypes.PdfLink:
-                    return "pdfLink";
-                case SourceTypes.Image:
-                    return "image";
-                case SourceTypes.ImageLink:
-                    return "imageLink";
-                default:
-                    throw new ArgumentException("No video type");
-            }
-        }
+            SourceTypes.Video => "video",
+            SourceTypes.VideoLink => "videoLink",
+            SourceTypes.Pdf => "pdf",
+            SourceTypes.PdfLink => "pdfLink",
+            SourceTypes.Image => "image",
+            SourceTypes.ImageLink => "imageLink",
+            _ => throw new ArgumentException("No video type")
+        };
     }
+
+    #endregion
 }
