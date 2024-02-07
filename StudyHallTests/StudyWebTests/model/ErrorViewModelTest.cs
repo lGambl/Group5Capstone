90eac1262c74570b82f994a3122e1c2fbbaa766b
Bryson Frederick <93341535+bfreder1@users.noreply.github.com>
2024-02-07 14:35:40 -0500

    Tested ErrorViewModel
    The ErrorViewModel web class has 100% coverage now.

---
+++ StudyHallTests/StudyWebTests/model/ErrorViewModelTest.cs
@@ -0,0 +1,27 @@
+﻿using StudyWeb.Models;
+
+namespace StudyHallTests.StudyWebTests.model;
+
+[TestFixture]
+public class ErrorViewModelTest
+{
+    [Test]
+    public void TestShowRequestId()
+    {
+        var errorViewModel = new ErrorViewModel
+        {
+            RequestId = "123"
+        };
+        Assert.That(errorViewModel.ShowRequestId, Is.True);
+    }
+
+    [Test]
+    public void TestShowRequestIdFalse()
+    {
+        var errorViewModel = new ErrorViewModel
+        {
+            RequestId = ""
+        };
+        Assert.That(errorViewModel.ShowRequestId, Is.False);
+    }
+}
