c429e00d68d0e0b4085c9a682d9534bc16057c9d
kmitche9 <74280045+kmitche9@users.noreply.github.com>
2024-02-06 20:52:06 -0500

    Added the ability to create a new user on the desktop app.
    I adjust the LoginForm to add a button to create a new user. I then created a new UserRegistrationForm. I then added the necessary code to make it function as expected.

--- StudyDesk/View/LoginForm.Designer.cs
+++ StudyDesk/View/LoginForm.Designer.cs
@@ -39,30 +39,34 @@ namespace StudyDesk.View
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
+            newUserButton = new Button();
             SuspendLayout();
             // 
             // UsernameTextBox
             // 
-            UsernameTextBox.Location = new Point(144, 151);
+            UsernameTextBox.Location = new Point(126, 113);
+            UsernameTextBox.Margin = new Padding(3, 2, 3, 2);
             UsernameTextBox.Name = "UsernameTextBox";
             UsernameTextBox.PlaceholderText = "Username";
-            UsernameTextBox.Size = new Size(125, 27);
+            UsernameTextBox.Size = new Size(110, 23);
             UsernameTextBox.TabIndex = 0;
             // 
             // PasswordTextBox
             // 
-            PasswordTextBox.Location = new Point(144, 208);
+            PasswordTextBox.Location = new Point(126, 156);
+            PasswordTextBox.Margin = new Padding(3, 2, 3, 2);
             PasswordTextBox.Name = "PasswordTextBox";
             PasswordTextBox.PasswordChar = '*';
             PasswordTextBox.PlaceholderText = "Password";
-            PasswordTextBox.Size = new Size(125, 27);
+            PasswordTextBox.Size = new Size(110, 23);
             PasswordTextBox.TabIndex = 1;
             // 
             // LoginButton
             // 
-            LoginButton.Location = new Point(160, 271);
+            LoginButton.Location = new Point(60, 201);
+            LoginButton.Margin = new Padding(3, 2, 3, 2);
             LoginButton.Name = "LoginButton";
-            LoginButton.Size = new Size(94, 37);
+            LoginButton.Size = new Size(82, 28);
             LoginButton.TabIndex = 2;
             LoginButton.Text = "Login";
             LoginButton.UseVisualStyleBackColor = true;
@@ -72,35 +76,47 @@ namespace StudyDesk.View
             // 
             label1.AutoSize = true;
             label1.Font = new System.Drawing.Font("Magneto", 25F);
-            label1.Location = new Point(69, 97);
+            label1.Location = new Point(60, 73);
             label1.Name = "label1";
-            label1.Size = new Size(277, 51);
+            label1.Size = new Size(226, 41);
             label1.TabIndex = 3;
             label1.Text = "StudyDesk";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(69, 155);
+            label2.Location = new Point(60, 116);
             label2.Name = "label2";
-            label2.Size = new Size(75, 20);
+            label2.Size = new Size(60, 15);
             label2.TabIndex = 4;
             label2.Text = "Username";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(69, 212);
+            label3.Location = new Point(60, 159);
             label3.Name = "label3";
-            label3.Size = new Size(70, 20);
+            label3.Size = new Size(57, 15);
             label3.TabIndex = 5;
             label3.Text = "Password";
             // 
+            // newUserButton
+            // 
+            newUserButton.Location = new Point(204, 201);
+            newUserButton.Margin = new Padding(3, 2, 3, 2);
+            newUserButton.Name = "newUserButton";
+            newUserButton.Size = new Size(82, 28);
+            newUserButton.TabIndex = 6;
+            newUserButton.Text = "New User";
+            newUserButton.UseVisualStyleBackColor = true;
+            newUserButton.Click += newUserButton_Click;
+            // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(431, 380);
+            ClientSize = new Size(377, 285);
+            Controls.Add(newUserButton);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -108,6 +124,7 @@ namespace StudyDesk.View
             Controls.Add(PasswordTextBox);
             Controls.Add(UsernameTextBox);
             ForeColor = Color.Black;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "LoginForm";
             Text = "LoginScreen";
             ResumeLayout(false);
@@ -122,5 +139,6 @@ namespace StudyDesk.View
         private Label label1;
         private Label label2;
         private Label label3;
+        private Button newUserButton;
     }
 }
---
+++ StudyWeb/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StudyWeb.Models;
+
+namespace StudyWeb.Controllers
+{
+    /// <summary>
+    ///   The Account Controller class.
+    /// </summary>
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="AccountController" /> class.
+        /// </summary>
+        /// <param name="userManager">The user manager.</param>
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        /// <summary>
+        ///   Registers the specified model.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        /// <returns>
+        ///   Succeeded if the registration was successful, bad request if unsuccessful
+        /// </returns>
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto model)
+        {
+            var user = new IdentityUser { UserName = model.Username, Email = model.Email, EmailConfirmed = true};
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok(new { message = "user created successfully" });
+            }
+
+            return BadRequest(result.Errors);
+        }
+    }
+}
