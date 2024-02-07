f3be1624ff625ab1f61a96d0841dff12cbf36e5a
Bryson Frederick <93341535+bfreder1@users.noreply.github.com>
2024-02-06 19:54:37 -0500

    Tested Note Model
    The Note class in the desktop Model package now has 100% test coverage.

--- StudyHallTests/StudyDeskTests/NoteTest.cs
+++ StudyHallTests/StudyDeskTests/NoteTest.cs
@@ -1,7 +1,20 @@
-﻿namespace StudyHallTests.StudyDeskTests;
+﻿using StudyDesk.Model;
+
+namespace StudyHallTests.StudyDeskTests;
 
 [TestFixture]
 public class NoteTest
 {
-    
+    [Test]
+    public void TestNoteConstructor()
+    {
+        var note = new Note(1, "test", 1, "test");
+        Assert.Multiple(() =>
+        {
+            Assert.That(note.Id, Is.EqualTo(1));
+            Assert.That(note.Text, Is.EqualTo("test"));
+            Assert.That(note.SourceId, Is.EqualTo(1));
+            Assert.That(note.Owner, Is.EqualTo("test"));
+        });
+    }
 }
