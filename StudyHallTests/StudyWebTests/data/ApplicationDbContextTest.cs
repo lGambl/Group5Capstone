c2cc7e1cbe0d559cb7d461d55eb7f6016855c380
Bryson Frederick <93341535+bfreder1@users.noreply.github.com>
2024-02-07 14:54:11 -0500

    Tested ApplicationDbContext
    The ApplicationDbContext now has 100% test coverage.

---
+++ StudyHallTests/StudyWebTests/data/ApplicationDbContextTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyWeb.Data;
+
+namespace StudyHallTests.StudyWebTests.data;
+
+[TestFixture]
+public class ApplicationDbContextTest
+{
+    [Test]
+    public void TestApplicationDbContext()
+    {
+        var context = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>());
+        Assert.That(context, Is.Not.Null);
+        Assert.That(context.Source, Is.Not.Null);
+        Assert.That(context.Note, Is.Not.Null);
+        Assert.That(context.SourceType, Is.Not.Null);
+    }
+}
--- StudyWeb/Data/ApplicationDbContext.cs
+++ StudyWeb/Data/ApplicationDbContext.cs
@@ -2,34 +2,33 @@
 using Microsoft.EntityFrameworkCore;
 using StudyWeb.Models;
 
-namespace StudyWeb.Data
+namespace StudyWeb.Data;
+
+/// <summary>
+///     The Database Context
+/// </summary>
+/// <remarks>
+///     The database context
+/// </remarks>
+/// <param name="options"></param>
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
 {
+    #region Properties
+
     /// <summary>
-    /// The Database Context
+    ///     The Source
     /// </summary>
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        /// <summary>
-        /// The database context
-        /// </summary>
-        /// <param name="options"></param>
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
-        /// <summary>
-        /// The Source
-        /// </summary>
-        public DbSet<Source> Source { get; set; } = default!;
+    public DbSet<Source> Source { get; init; } = default!;
 
-        /// <summary>
-        /// The Note
-        /// </summary>
-        public DbSet<Note> Note { get; set; } = default!;
+    /// <summary>
+    ///     The Note
+    /// </summary>
+    public DbSet<Note> Note { get; init; } = default!;
 
-        /// <summary>
-        /// The SourceType
-        /// </summary>
-        public DbSet<SourceType> SourceType { get; set; } = default!;
-    }
+    /// <summary>
+    ///     The SourceType
+    /// </summary>
+    public DbSet<SourceType> SourceType { get; init; } = default!;
+
+    #endregion
 }
