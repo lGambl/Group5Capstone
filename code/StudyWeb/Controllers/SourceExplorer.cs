2c97b77ed91a4a3d3a9c653f92d81221d345f3d4
kmitche9 <74280045+kmitche9@users.noreply.github.com>
2024-02-19 15:06:44 -0500

    Added functionality to delete notes on the web app.
    I added a delete button in the HTML for each note. Next, I added the necessary Javascript to hit the DeleteNote method that I added in SourceExplorer. Finally, I updated the Javascript to add the delete note button automatically when a new note is created and to refresh the page.

--- code/StudyWeb/Controllers/SourceExplorer.cs
+++ code/StudyWeb/Controllers/SourceExplorer.cs
@@ -390,5 +390,37 @@ public class SourceExplorer : Controller
         return Ok(new { success = true, message = "The source has no notes to delete." });
     }
 
+    /// <summary>
+    /// Deletes the note.
+    /// </summary>
+    /// <param name="noteId">The note identifier.</param>
+    /// <returns>
+    ///   Ok, if the note was removed successfully.
+    ///   BadRequest if the note was not deleted.
+    /// </returns>
+    [Authorize]
+    [HttpDelete]
+    [Route("DeleteNote/{noteId}")]
+    public async Task<IActionResult> DeleteNote(int noteId)
+    {
+        if (noteId < 0)
+        {
+            return BadRequest(new { success = false, message = "Note Id not found. " });
+        }
+
+        try
+        {
+            const string deleteSourceQuery = "DELETE FROM note WHERE Id = @Id";
+            var parameter = new SqlParameter("@Id", noteId);
+            await this.context.Database.ExecuteSqlRawAsync(deleteSourceQuery, parameter);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { success = false, message = ex.Message });
+        }
+
+        return Ok(new { success = true, message = "Note deleted successfully." });
+    }
+
     #endregion
 }
