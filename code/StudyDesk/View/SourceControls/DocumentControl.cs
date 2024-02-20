7111c5c9d75e23f390f712d728c7f51634087036
lGambl <110194340+lGambl@users.noreply.github.com>
2024-02-20 14:31:25 -0500

    Modify pdf viewing

--- code/StudyDesk/View/SourceControls/DocumentControl.cs
+++ code/StudyDesk/View/SourceControls/DocumentControl.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.RegularExpressions;
+﻿using PdfiumViewer;
 
 namespace StudyDesk.View.SourceControls;
 
@@ -8,13 +8,18 @@ namespace StudyDesk.View.SourceControls;
 /// <seealso cref="System.Windows.Forms.UserControl" />
 public partial class DocumentControl : UserControl
 {
+
+    private double zoomFactor = 1.0;
+    private string filePath;
+
     /// <summary>
     ///     Initializes a new instance of the <see cref="DocumentControl" /> class.
     /// </summary>
     public DocumentControl()
     {
         this.InitializeComponent();
-        this.documentViewer1.PerformAutoScale();
+        this.filePath = "";
+        flowLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
     }
 
     /// <summary>
@@ -25,15 +30,14 @@ public partial class DocumentControl : UserControl
     {
         try
         {
-            
             var trimmedFile = filename.Split("/")[^1];
             var trimmedFilename = trimmedFile.Substring(8);
-            
+
             var temp = Path.Combine(Path.GetTempPath(), trimmedFilename);
-            if (downloadFileAsync(filename, temp).Result)
+            if (DownloadFileAsync(filename, temp).Result)
             {
-                var stream = new FileStream(temp, FileMode.Open, FileAccess.Read);
-                this.documentViewer1.LoadDocument(stream);
+                this.filePath = temp;
+                this.RenderPdf();
                 return Task.FromResult(true);
             }
 
@@ -46,7 +50,30 @@ public partial class DocumentControl : UserControl
         }
     }
 
-    private static async Task<bool> downloadFileAsync(string fileUrl, string destinationPath)
+
+    private void RenderPdf()
+    {
+        flowLayoutPanel1.Controls.Clear();
+
+        using var document = PdfDocument.Load(this.filePath);
+        for (int pageIndex = 0; pageIndex < document.PageCount; pageIndex++)
+        {
+            var image = document.Render(pageIndex, (int)(300 * zoomFactor), (int)(300 * zoomFactor), true);
+            var pictureBox = new PictureBox
+            {
+                Image = image,
+                SizeMode = PictureBoxSizeMode.Zoom,
+                Width = flowLayoutPanel1.ClientSize.Width,
+                Height = (int)(image.Height * ((double)flowLayoutPanel1.ClientSize.Width / image.Width)),
+                Margin = new Padding(0)
+            };
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.Controls.Add(pictureBox);
+        }
+    }
+
+
+    private static async Task<bool> DownloadFileAsync(string fileUrl, string destinationPath)
     {
         try
         {
@@ -78,5 +105,39 @@ public partial class DocumentControl : UserControl
         {
             return false;
         }
+    }
+
+    /// <summary>
+    ///     Zooms the in the pdf.
+    /// </summary>
+    public void ZoomIn()
+    {
+        zoomFactor *= 1.1;
+        if (flowLayoutPanel1.Controls.Count > 0)
+        {
+            RenderPdf();
+        }
+    }
+
+    /// <summary>
+    ///     Zooms the out the pdf.
+    /// </summary>
+    public void ZoomOut()
+    {
+        zoomFactor /= 1.1;
+        if (flowLayoutPanel1.Controls.Count > 0)
+        {
+            RenderPdf();
+        }
+    }
+
+    private void zoomInButton_Click(object sender, EventArgs e)
+    {
+        this.ZoomIn();
+    }
+
+    private void zoomOutButton_Click(object sender, EventArgs e)
+    {
+        this.ZoomOut();
     }
 }
