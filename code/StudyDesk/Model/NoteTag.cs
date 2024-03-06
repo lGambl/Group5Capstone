c2663d4b7c2007c3ebd2e0cd045eb2c2072a1580
Bryson Frederick <93341535+bfreder1@users.noreply.github.com>
2024-03-06 18:56:08 -0500

    Fixed Directory naming issues
    There was a breakdown in directory naming that caused development code to get scattered across two different folders and documentation to be duped.

---
+++ code/StudyDesk/Model/NoteTag.cs
@@ -0,0 +1,27 @@
+﻿namespace StudyDesk.Model
+{
+    /// <summary>
+    ///   The Note Tag class.
+    /// </summary>
+    public class NoteTag
+    {
+        /// <summary>
+        ///   Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public int Id { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the note identifier.
+        /// </summary>
+        /// <value>The note identifier.</value>
+        public int NoteId { get; set; }
+
+        /// <summary>
+        ///   Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string? Name { get; set; }
+
+    }
+}
